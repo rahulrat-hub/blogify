e8c374f2660c5831048621e9d376e01777baca3a
Rahul Raturi <rahulraturi84@gmail.com>
2026-06-18 21:51:30 +0530

    blog frontend

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from "react";
+import bgr from "../assets/bgr.png";
 
 function Blog() {
   return (
-    <div>Blog</div>
-  )
+    <div className="min-screen w-full relative">
+      <img src={bgr} alt="" className="h-full w-full" />
+
+      <div className="absolute top-2 left-100 border-2 rounded-[10px] my-6">
+        <h1 className="text-[20px] font-bold tracking-wide p-4">
+          Share Your Ideas With The World.
+        </h1>
+
+        <form
+          className="p-4 space-y-6 backdrop-blur-md
+         bg-white/20 h-105 w-120 rounded-[10px] "
+        >
+          {/* <input
+            type="text"
+            className="w-full border rounded-[10px] p-2 outline-none"
+            placeholder="Enter Your Image"
+          /> */}
+
+          <div className="border-2 border-dashed border-violet-400 rounded-xl p-10 flex flex-col items-center justify-center cursor-pointer hover:bg-violet-50 transition">
+            <label
+              htmlFor="image-upload"
+              className="cursor-pointer flex flex-col items-center"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-12 h-12 text-violet-600"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14"
+                />
+              </svg>
+
+              <p className="mt-3 font-semibold">Drag & Drop image here</p>
+
+              <p className="text-gray-500 text-sm">or click to upload</p>
+            </label>
+
+            <input id="image-upload" type="file" className="hidden" />
+          </div>
+
+          <input
+            type="text"
+            className="w-full border rounded-[10px] p-2 outline-none"
+            placeholder="Enter Your Title"
+          />
+
+          <input
+            type="text"
+            className="w-full border rounded-[10px] p-2 outline-none"
+            placeholder="Enter Your Description"
+          />
+
+          <div className="flex gap-6 px-28">
+            <button className="border rounded-[5px] p-2 font-bold bg-gray-200 cursor-pointer">Save Blog</button>
+
+          <button className="border rounded-[5px] p-2 bg-violet-500 font-bold cursor-pointer">Pulish Blog</button>
+          </div>
+
+       </form>
+      </div>
+    </div>
+  );
 }
 
-export default Blog
+export default Blog;
